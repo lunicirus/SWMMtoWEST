1cb0dd42848bd54cd82530f13763fbfeae07109b
lunicirus <lunicirus@gmail.com>
2024-03-15 12:59:13 -0400

    slopes test

--- tests/getNetworkFromSWMM_Test.py
+++ tests/getNetworkFromSWMM_Test.py
@@ -1,6 +1,7 @@
 import pytest
 import swmmio
 import pandas as pd
+import numpy as np
 
 from SWMMToWESTConvert import getNetworkFromSWMM as gnfs
 from SWMMToWESTConvert import SWMM_InpConstants as SWWM_C
@@ -32,10 +33,14 @@ def test_getLinksWithSlope(sample_model):
     expected_columns = [SWWM_C.IN_NODE,SWWM_C.OUT_NODE,SWWM_C.LEN,SWWM_C.DIAM,SWWM_C.MAX_Q,SWWM_C.ROUG,SWWM_C.SHAPE,STW_C.SLOPE]
     assert result.columns.tolist() == expected_columns
     
+    #Gets the expected slopes and truncate them 
+    expected_slopes_file = "tests/PCSWMMSlopes.csv"
+    expected_slope = pd.read_csv(expected_slopes_file, delimiter = ',',index_col=['Name'])
+    resultCompa = result.join(expected_slope)
+
     # Check if the slope is calculated correctly
-    
-    #expected_slope = [(200 - 100) / 100, (250 - 150) / 150] #TODO!!
-    #assert result['slope'].tolist() == expected_slope
+    for expected_val, actual_val, pipe in zip(resultCompa["Slope (m/m)"].to_list(), resultCompa[STW_C.SLOPE].tolist(),resultCompa.index.to_list()):
+        assert np.isclose(expected_val, actual_val, rtol=0.1), f"Values are not within 10% tolerance: ({pipe}) {expected_val}, {actual_val}"
     
     # Check if columns SWWM_C.INOFF and SWWM_C.OUTOFF are dropped
     assert SWWM_C.INOFF not in result.columns
